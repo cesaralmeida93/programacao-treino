2d35c73cda66640d7ccec446114f6852cf074b97
Cesar <ocesaralmeida@gmail.com>
2022-06-18 08:28:05 -0300

    first go exercise

---
+++ codewars/8-kyu/string-repeat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func RepeatStr(repetitions int, value string) string {
+  return strings.Repeat(value, repetitions)
+}
+
+func main() {
+	fmt.Println(RepeatStr(6, "I"))
+	fmt.Println(RepeatStr(5, "Hello"))
+}
