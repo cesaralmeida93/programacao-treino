e4c4b648d2ac69444da8856b873aa5739fe89cd8
Cesar <ocesaralmeida@gmail.com>
2022-06-28 06:30:47 -0300

    addded new solution to multiply problem

--- codewars/8-kyu/2-multiply/multiply.go
+++ codewars/8-kyu/2-multiply/multiply.go
@@ -3,3 +3,20 @@ package multiply
 func Multiply(a, b int) int {
 	return a * b
 }
+
+// package multiply
+
+// type fattori struct {
+
+//   fattore1 int
+//   fattore2 int
+
+// }
+
+// func Multiply(a, b int) int {
+
+//   moltiplicazione := fattori{ fattore1 : a,
+//                               fattore2 : b }
+
+//   return moltiplicazione.fattore1 * moltiplicazione.fattore2
+// }
