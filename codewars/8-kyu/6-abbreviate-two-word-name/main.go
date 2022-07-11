347a96bf1ed932502481a43da80097771e0cd6b7
Cesar <ocesaralmeida@gmail.com>
2022-07-11 09:04:39 -0300

    finished exercise6 8kyu

---
+++ codewars/8-kyu/6-abbreviate-two-word-name/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func AbbrevName(name string) string{
+	words := strings.Split(name, " ")
+	fmt.Println(string(words[0][0]))
+	res := ""
+	res += string(words[0][0])
+	fmt.Println(string(words[0][0]))
+	res += "."
+	res += string(words[1][0])
+	fmt.Println(string(words[1][0]))
+  return strings.ToUpper(res)
+}
+
+
+//-------TRAINING-------
+// func AbbrevName(name string) string{
+// 	words := strings.Split(name, " ")
+// 	fmt.Println(string(words[0][0]))
+// 	res := ""
+// 	res += string(words[0][0])
+// 	fmt.Println(string(words[0][0]))
+// 	res += "."
+// 	res += string(words[1][0])
+// 	fmt.Println(string(words[1][0]))
+//   return strings.ToUpper(res)
+// }
+
+//-------BEST PRACTICE-------
+// package kata
+
+// import "strings"
+
+// func AbbrevName(name string) string{
+//   words := strings.Split(name, " ")
+//   return strings.ToUpper(string(words[0][0])) + "." + strings.ToUpper(string(words[1][0]))
+// }
+
+func main() {
+	fmt.Println(AbbrevName("Sam Harris"))
+}
