599f4f66585dc4fc9cad53a636ae93a1b7b17284
Cesar <ocesaralmeida@gmail.com>
2022-07-16 10:50:07 -0300

    finished 10 reversed-strings

---
+++ codewars/8-kyu/10-Reversed-strings/README.md
@@ -0,0 +1,8 @@
+# Messi goals function
+
+Complete the solution so that it reverses the string passed into it.
+
+```
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+```
---
+++ codewars/8-kyu/10-Reversed-strings/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func Solution(word string) (result string) {
+	for _, v := range word {
+		result = string(v) + result
+	}
+	return
+
+}
+
+
+//-------TRAINING-------
+// func Solution(word string) (result string) {
+// 	for i, v := range word {
+// 		fmt.Println("index: ", i)
+// 		fmt.Println("value before string: ", v)
+// 		fmt.Println("value after string: ", string(v))
+// 		fmt.Println("result before: ", result)
+// 		result = string(v) + result
+// 		fmt.Println("result after: ", result)
+// 		fmt.Println("")
+// 	}
+// 	return result
+// }
+
+func main() {
+	fmt.Println(Solution("teste"))
+	// fmt.Println(Goals(43, 10, 5))
+	// teste := "teste"
+	// byte_str := []rune(teste)
+	// fmt.Println(byte_str)
+}
