66e029c47dec9098775741d153c249ba9a3309ef
César Almeida <cesar.almeida@vocedm.com.br>
2023-08-14 13:08:15 -0300

    finished 7kyu baby shark lyrics generator exercise

--- codewars/7-kyu/5-baby-shark-lyrics-generator/main.go
+++ codewars/7-kyu/5-baby-shark-lyrics-generator/main.go
@@ -1,6 +1,38 @@
 package kata
 
-func BabySharkLyrics() string {
-	// good luck!
-	return ""
+const S = ", doo doo doo doo doo doo\n"
+
+func BabySharkLyrics() (r string) {
+
+	a := func(p string) { r += p + S + p + S + p + S + p + "!\n" }
+	b := func(p string) { a(p + " shark") }
+
+	b("Baby")
+	b("Mommy")
+	b("Daddy")
+	b("Grandma")
+	b("Grandpa")
+	a("Let's go hunt")
+
+	return r + "Run away,…\n"
 }
+
+// package kata
+// import s "strings"
+// func BabySharkLyrics() (r string) {
+// 	z := s.Repeat
+// 	for _, i := range []string{"Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"} {
+// 		r += z(i+","+z(" doo", 6)+"\n", 3) + i + "!\n"
+// 	}
+// 	return r + "Run away,…\n"
+// }
+
+// package kata
+// import"strings"
+// func BabySharkLyrics()string{
+// t:="B,B,B,B!M,M,M,M!D,D,D,D!Gm,Gm,Gm,Gm!Gp,Gp,Gp,Gp!L,L,L,L!R"
+// for _,R:=range strings.Split(`!
+// $Ds$ma$pa$a s$, d d d d d d
+// $Run away,…
+// $doo$Baby s$Mommy s$shark$Grand$Daddy $Let's go hunt`,"$"){t=strings.ReplaceAll(t,R[:1],R)}
+// return t}
